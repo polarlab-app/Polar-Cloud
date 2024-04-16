82ce157d775ade1b65eb99d25d50650914b042a5
Aertic <63727339+Aertic@users.noreply.github.com>
2024-04-16 17:33:08 +0200

    Added some stuff to the controlPanel CSS and fixed JS file

--- components/controlPanel.jsx
+++ components/controlPanel.jsx
@@ -2,43 +2,37 @@
 
 import { useState } from 'react';
 import styles from '@css/home/panel.module.css';
-import createFile from '@/lib/files/createFile';
-import createFolder from '@/lib/files/createFolder';
+import CreateFile from './modals/createFile';
+import CreateFolder from './modals/createFolder';
 import { useData } from '@/app/home/context';
 export default function ControlPanel() {
     const { dir, setDir } = useData();
+    const [modal, setModal] = useState(null);
 
     return (
-        <div className={styles.panel}>
-            <h2 className={styles.directory}>Testing My Ass</h2>
-            <div className={styles.dropdown}>
-                <div className={styles.selector}>
-                    New
-                    <ul className={styles.options}>
-                        <li className={styles.option} onClick={() => setFolderName(prompt('Enter folder name:'))}>
-                            <p className={styles.optiontext} onClick={handleCreateFolder}>
-                                Create Folder
-                            </p>
-                        </li>
-                        <li
-                            className={styles.option}
-                            onClick={() => {
-                                setFileName(prompt('Enter file name:'));
-                                setFileExtension(prompt('Enter file extension:'));
-                            }}>
-                            <p className={styles.optiontext} onClick={handleCreateFile}>
-                                Create File
-                            </p>
-                        </li>
-                        <li className={styles.option}>
-                            <p className={styles.optiontext}>Upload Folder</p>
-                        </li>
-                        <li className={styles.option}>
-                            <p className={styles.optiontext}>Upload File</p>
-                        </li>
-                    </ul>
+        <>
+            <div className={styles.panel}>
+                <h2 className={styles.directory}>{dir}</h2>
+                <div className={styles.dropdown}>
+                    <div className={styles.selector}>
+                        New
+                        <ul className={styles.options}>
+                            <li className={styles.option} onClick={() => setModal(<CreateFolder />)}>
+                                <p className={styles.optiontext}>Create Folder</p>
+                            </li>
+                            <li className={styles.option} onClick={() => setModal(<CreateFile />)}>
+                                <p className={styles.optiontext}>Create File</p>
+                            </li>
+                            <li className={styles.option}>
+                                <p className={styles.optiontext}>Upload Folder</p>
+                            </li>
+                            <li className={styles.option}>
+                                <p className={styles.optiontext}>Upload File</p>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
