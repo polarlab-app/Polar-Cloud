5655f1511e8cbd8222cd263849333c12ec4601cf
Aertic <63727339+Aertic@users.noreply.github.com>
2024-04-16 16:28:02 +0200

    Added createFolder modal

---
+++ components/modals/createFolder.jsx
@@ -0,0 +1,26 @@
+'use client';
+import { useState } from 'react';
+import createFolder from '@/lib/files/createFolder';
+import styles from '@css/home/modals.module.css';
+
+export default function CreateFolder(dir) {
+    const [folderName, setFolderName] = useState('');
+
+    async function submitFolder() {
+        e.preventDefault();
+        await createFolder(dir, folderName);
+    }
+
+    return (
+        <div className={styles.modal}>
+            <h2 className={styles.header}>Create Folder</h2>
+            <div className={styles.inputcontainer}>
+                <label className={styles.label}>Folder Name</label>
+                <input type='text' placeholder='Folder Name' onChange={(e) => setFolderName(e.target.value)}></input>
+            </div>
+            <button className={styles.button} onClick={submitFolder}>
+                Create
+            </button>
+        </div>
+    );
+}
